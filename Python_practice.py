4834b62674a3bc7f579258a9a342ac07455d03d6
Steve MacPherson <stevemacpherson@rogers.com>
2021-05-17 00:48:43 -0400

    Adding Election Analysis.

--- Python_practice.py
+++ Python_practice.py
@@ -15,4 +15,4 @@ counties = ["Arapahoe","Denver","Jefferson"]
 if "El Paso" in counties:
     print("El Paso is in the list of counties.")
 else:
-    print("El Paso is not the list of counties.")
+    print("El Paso is not the list of counties.")44
